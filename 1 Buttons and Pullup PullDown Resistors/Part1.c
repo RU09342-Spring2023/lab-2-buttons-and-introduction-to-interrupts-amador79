5779c98eb2590fc3cdb291fea32f2c7f5613fbd2
Caleb Amadoro <amador79@students.rowan.edu>
2023-02-02 17:06:18 -0500

    Nevermind part 1 didnt work but now it does

--- 1 Buttons and Pullup PullDown Resistors/Part1.c
+++ 1 Buttons and Pullup PullDown Resistors/Part1.c
@@ -34,7 +34,7 @@ int main(void)
     {
         if (!(P2IN & BIT3))                // If S2 (P2.3) is not pressed
             P6OUT ^= BIT6;                 // Toggle P6.6
-        if (!(P4IN & BIT1))                // If S1 (P4.1) is not pressed
+        else if (!(P4IN & BIT1))                // If S1 (P4.1) is not pressed
             P1OUT ^= BIT0;                 // Toggle P1.0
         else{
             P6OUT &= ~BIT6;                // Turns green LED off when nothing is pressed
