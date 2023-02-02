1a4c240f21c1c99d20432af9f6f8ae8de92f2570
Caleb Amadoro <amador79@students.rowan.edu>
2023-02-02 17:00:37 -0500

    Made part 1 more efficient

--- 1 Buttons and Pullup PullDown Resistors/Part1.c
+++ 1 Buttons and Pullup PullDown Resistors/Part1.c
@@ -32,9 +32,9 @@ int main(void)
 
     while(1)
     {
-        if ((P2IN & BIT3) == 0)            // If S2 (P2.3) is not pressed
+        if (!(P2IN & BIT3))                // If S2 (P2.3) is not pressed
             P6OUT ^= BIT6;                 // Toggle P6.6
-        if ((P4IN & BIT1) == 0)            // If S1 (P4.1) is not pressed
+        if (!(P4IN & BIT1))                // If S1 (P4.1) is not pressed
             P1OUT ^= BIT0;                 // Toggle P1.0
         else{
             P6OUT &= ~BIT6;                // Turns green LED off when nothing is pressed
